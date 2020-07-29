17f9286c10f28243cad13633e38e3eb8a2e70415
moomoocowjonny <moomoocowjonny@gmail.com>
2020-07-29 09:48:19 -0700

    Updates

---
+++ precache-manifest.f63663b4a3c8f1fc45c6ae01ba2db585.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ac1039675ee675189b77eb5cc063b4",
+    "url": "/React-Deploy/index.html"
+  },
+  {
+    "revision": "cb018e85824b084ef8aa",
+    "url": "/React-Deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8b58dfabffa3d3b113df",
+    "url": "/React-Deploy/static/js/2.77d6aa3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Deploy/static/js/2.77d6aa3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb018e85824b084ef8aa",
+    "url": "/React-Deploy/static/js/main.7b32ff9c.chunk.js"
+  },
+  {
+    "revision": "1cfa8e9f61f25784c5f0",
+    "url": "/React-Deploy/static/js/runtime-main.5d657504.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
